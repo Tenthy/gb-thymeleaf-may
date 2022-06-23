8e9b174e106097357e6e2120154e3500c71416e3
Kmetha <rakursdead@gmail.com>
2022-06-23 15:20:45 +0300

    Initial commit

---
+++ src/main/java/ru/kmetha/gbthymeleafmay/GbThymeleafMayApplication.java
@@ -0,0 +1,13 @@
+package ru.kmetha.gbthymeleafmay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbThymeleafMayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbThymeleafMayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kmetha/gbthymeleafmay/GbThymeleafMayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kmetha.gbthymeleafmay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbThymeleafMayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
